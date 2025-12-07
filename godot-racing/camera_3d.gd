297cdce5d0c5b382a7f372f38b0145caa9e33b0b
elonmusksclone <yosif.carroll@gmail.com>
2025-12-07 13:10:43 -0500

    Inital commit

---
+++ godot-racing/camera_3d.gd
@@ -0,0 +1,23 @@
+extends Camera3D
+var a=0	
+var speed = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.z=position.z+sin(a)/10*speed
+	position.x=position.x+cos(a)/10*speed
+	if (Input.is_action_pressed("KEYA")):
+		a=a+0.1
+	if (Input.is_action_pressed("KEYD")):
+		a=a-0.1
+	if (Input.is_action_pressed("KEYW")):
+		speed=speed+0.1
+	if (Input.is_action_pressed("KEYS")):
+		speed=speed-0.1
+	await get_tree().create_timer(2).timeout
