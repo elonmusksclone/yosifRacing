ab55f40b21bcd634d1bca627b6f08c454c2b39f6
elonmusksclone <yosif.carroll@gmail.com>
2026-07-26 14:33:57 -0400

    Added placement and money tracking

--- godot-racing/global.gd
+++ godot-racing/global.gd
@@ -1,9 +1,42 @@
 extends Node3D
 
-var usoilprice = 25.66
+var oilprice = 25.66
 var inflation = 1.03
-var meoilprice
-var coilprice
-var euoilprice
-var roilprice = 100
 var taxrate = 0
+var a=0	
+var damp=0.5
+var olda=0
+#a is for angle
+var speed = 0
+var acc = 0.03
+var playerx = 0
+var playerz = 0
+var cash = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	playerz=playerz+sin(olda)/5*speed
+	playerx=playerx+cos(olda)/5*speed
+	rotate_object_local(Vector3(0, 1, 0), -a)
+	olda=olda+a
+	a=0
+	speed=speed*0.99
+	if (Input.is_action_pressed("KEYA")):
+		a=a-(0.04*speed*damp)
+	if (Input.is_action_pressed("KEYD")):
+		a=a+(0.04*speed*damp)
+	if (Input.is_action_pressed("KEYW")):
+		speed=speed+acc
+	if (Input.is_action_pressed("KEYS")):
+		speed=speed-acc
+	if (Input.is_action_pressed("KEYC")):
+		playerz=playerz+sin(olda)/5*-speed
+		playerx=playerx+cos(olda)/5*-speed
+		speed=-(speed/1.3)
+	await get_tree().create_timer(2).timeout
